c69101b99e4c15b871b5c352a80f89fc7964d057
Student <student@RU-017.local>
2015-10-28 17:06:43 -0600

    day2

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get('/about', function(req, res){
 
 
 // Creating Server and Listening for Connections \\
-var port = 3000
+var port = 80
 app.listen(port, function(){
   console.log('Server running on port ' + port);
 
